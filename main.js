1bcf82d2f1fb32548387ee2cfa8043c874f2df31
Amarakalyan <86305543+Amarakalyan@users.noreply.github.com>
2022-01-04 18:33:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+img="";
+
+function preload()
+{
+    img=loadImage("dog_cat.jpg");
+}
+
+function setup()
+{
+  canvas=createCanvas(640,420);
+  canvas.center();
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#ff0000");
+    text("Dog",45,75);
+    noFill();
+    stroke("#ff0000");
+    rect(30,60,500,350);
+
+    stroke("#ff80ff");
+    fill("#ffffff");
+    noFill();
+    rect(300,70,280,330);
+    text("Cat",330,120);
+
+}
